8d88ac93ef39c9a66871af9b001ea8aeeabdbd01
vivekpanth <vivekpanth09@gmail.com>
2023-08-20 13:34:56 +0530

    changed some js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const typeofconversion=document.getElementById("conversiontype")
 const inputconversion=document.querySelector(".conversion")
 const outputconversion=document.getElementById("convertedresult")
-
+  
 typeofconversion.addEventListener('change',)
