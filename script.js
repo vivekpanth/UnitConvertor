1612bfe70f685e50ddc4c6db20651c2f8bf980fa
vivekpanth <vivekpanth09@gmail.com>
2023-08-21 08:56:49 +0530

    added conversion types

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ const typeofconversion=document.getElementById("conversiontype")
 const inputconversion=document.querySelector(".conversion")
 const outputconversion=document.getElementById("convertedresult")
   
-typeofconversion.addEventListener('change',)
+typeofconversion.addEventListener('change',createconversionsinputs)
+
+const createconversionsinputs=()=>{
+    const selectedtype=typeofconversion.value
+    inputconversion.innerHTML=""
+    if(selectedtype=="length"){
+       createlengthinputs();
+    }
+    if(selectedtype=="mass"){
+       createmassinputs();
+    }
+    if(selectedtype=="temperature"){
+       createtemperatureinputs();
+    }
+    if(selectedtype=="time"){
+       createtimeinputs();
+    }
+
+}
